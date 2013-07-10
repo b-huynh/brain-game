5e6d78964d9ebfc6712608cc07912e986dcbe2e4
Calvin Phung <RynsArgent@hotmail.com>
2013-07-10 02:36:02 -0700

    Updated Tunnel Class
    Cleaned up unnecessary compiled objects. Replaced Direction.h with
    Util.h. Added additional textures. Started Pattern class. Tunnel now
    infinitely loops when run.

--- Vinezors/Windows/Vine.cpp
+++ Vinezors/Windows/Vine.cpp
@@ -8,12 +8,27 @@ Vine::Vine(CollisionScene *scene, Vector3 pos, Number length, Number radius)
 	: tip(NULL), dest()
 {
 	tip = new ScenePrimitive(ScenePrimitive::TYPE_CONE, length, radius, 10);
-	tip->setPitch(-45);
+	tip->setPitch(-90);
 	tip->setPosition(pos);
 	//tip->setMaterialByName("VineMaterial");
 	tip->loadTexture("resources/green_solid.png");
 
 	addToCollisionScene(scene);
+}
+
+ScenePrimitive *Vine::getTip() const
+{
+	return tip;
+}
+
+Vector3 Vine::getPos() const
+{
+	return tip->getPosition();
+}
+
+void Vine::setPos(Vector3 value)
+{
+	tip->setPosition(value);
 }
 
 Vector3 Vine::getDest() const
@@ -33,6 +48,25 @@ void Vine::move(Vector3 delta)
 
 void Vine::update(Number elapsed)
 {
+	const Number VINE_SPEED = 100.0;
+
+	Vector2 dist = Vector2(dest.x - tip->getPosition().x, dest.y - tip->getPosition().y);
+	
+	if (dist.length() < 0.1)
+	{
+		dist.x = 0;
+		dist.y = 0;
+	}
+	Vector2 norm = dist;
+	norm.Normalize();
+	tip->setPitch(-90 + norm.y * 180 / PI);
+	tip->setRoll(norm.x * 180 / PI);
+	Vector2 move = norm * VINE_SPEED;
+	if (move.length() > dist.length())
+		move = dist;
+	tip->Translate(Vector3(move.x, move.y, 0.0));
+	
+	//tip->Translate(Vector3(dist.x, dist.y, 0.0));
 }
 
 void Vine::addToCollisionScene(CollisionScene *scene)
